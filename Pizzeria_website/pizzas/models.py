8b9728f71e059c410183e7f24911f6f3dfcdeaf2
VladimirGHG <vlad2005garag@Gmail.com>
2024-08-07 21:10:03 +0400

    Added view, and pizzas

--- Pizzeria_website/pizzas/models.py
+++ Pizzeria_website/pizzas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
+    score = models.PositiveIntegerField()
+    details = models.TextField(max_length=300)
+
+    def __str__(self):
+        return f"Pizza {self.name}"
