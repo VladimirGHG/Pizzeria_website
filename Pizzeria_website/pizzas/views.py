167c44f97e87bfaedcae640ba955cdf21bb06891
VladimirGHG <vlad2005garag@Gmail.com>
2024-08-11 18:43:36 +0400

    Added pagination, and little functional tools

--- Pizzeria_website/pizzas/views.py
+++ Pizzeria_website/pizzas/views.py
@@ -1,13 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from .models import Pizza
 
 
 def home(requests):
-    context = {
-        'pizzas': Pizza.objects.all()
-    }
-    return render(requests, 'pizzas/home.html', context)
+    context = Pizza.objects.all()
+    paginator = Paginator(context, 6)  # Show 5 pizzas per page.
+
+    page_number = requests.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(requests, 'pizzas/home.html', {'pizzas': context, 'page_obj': page_obj})
 
 
 def pizza_detail(request, id):
